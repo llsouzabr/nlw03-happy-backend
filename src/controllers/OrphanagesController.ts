4a78a7e3be7c35bfe3b95a98ccd950ede9775165
Leonardo Souza <llsouza.br@gmail.com>
2020-10-13 23:41:12 -0300

    Manipulando retorno da API com views

--- src/controllers/OrphanagesController.ts
+++ src/controllers/OrphanagesController.ts
@@ -2,21 +2,26 @@ import {Request, Response} from 'express';
 import {getRepository} from 'typeorm';
 
 import Orphanages from '../models/Orphanage';
+import orphanageView from '../views/orphanagesView';
 
 export default {
   async index(req: Request, res:Response) {
     const orphanagesRepository = getRepository(Orphanages);
-    const orphanages = await orphanagesRepository.find();
+    const orphanages = await orphanagesRepository.find({
+      relations: ['images']
+    });
 
-    return res.status(200).json(orphanages);
+    return res.status(200).json(orphanageView.renderMany(orphanages));
   },
 
   async show(req: Request, res:Response) {
     const { id } = req.params;
     const orphanagesRepository = getRepository(Orphanages);
-    const orphanage = await orphanagesRepository.findOneOrFail(id);
+    const orphanage = await orphanagesRepository.findOneOrFail(id,{
+      relations: ['images']
+    });
 
-    return res.status(200).json(orphanage);
+    return res.status(200).json(orphanageView.render(orphanage));
   },
 
 
